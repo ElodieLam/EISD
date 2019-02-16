0b014283a49adb8b6e62a99ce51f15123e93b07f
= <anaisprog@gmail.com>
2019-02-16 17:53:41 +0100

    ajout de la distance de leven

--- dark-2.3.4/analyseurQuestionLua.lua
+++ dark-2.3.4/analyseurQuestionLua.lua
@@ -1,6 +1,11 @@
 dark = require("dark")
 
 local P = dark.pipeline()
+local t = {}
+
+listObjectifs = {"equilibre", "coordination", "endurance", "tonicité", "détente", "vélocité", "réflexes", "concentration", "perte de poids", "poids"}
+listGenerations = {"jeune", "18 ans", "personne agée", "agé", "jeune"}
+listMaladies = {"asthme", "asthmatique", "enceinte", "os fragiles", "sclerose en plaques"}
 
 P:basic()
 P:lexicon("#sport", {"natation", "basket - ball", "yoga", "sport", "sports"})
@@ -9,11 +14,11 @@ P:lexicon("#bien", {"avantages", " bienfaits", "" })
 P:lexicon("#maladie", {"asthme", "asthmatique", "enceinte", "os fragiles", "sclerose en plaques"})
 P:lexicon("#but", {"but", "objectif"})
 --ajout 15/02
-P:lexicon("#listeObjectifs", {"equilibre", "coordination", "endurance", "tonicité", "détente", "vélocité", "réflexes", "concentration", "perte de poids", "poids"})
+P:lexicon("#listeObjectifs", listObjectifs)
 
-P:lexicon("#listeGenerations", {"jeune", "personne agée", "agé", "jeune"})
+P:lexicon("#listeGenerations", listGenerations)
 
-P:lexicon("#listesMaladies", {"asthme", "asthmatique", "enceinte", "os fragiles", "sclerose en plaques"})
+P:lexicon("#listesMaladies",listMaladies)
 
 
 
@@ -31,7 +36,6 @@ P:pattern([[ [#precisionQ #precision] ]])
 P:pattern([[ [#butQ #question] ]])
 P:pattern([[ [#equipement   ("vous" "faut" | "faut" | "besoin" | "équipement")] ]])
 P:pattern([[ [#age ("âge" | "jeune" | "adulte" | #d "ans")] ]])
-
 
 
 
@@ -71,7 +75,6 @@ function tagstring(seq, tag, deb, fin)
 end
 
 
-
 function tagstringlink(seq, link, tag)
 	if not havetag(seq, link) then
 		return
@@ -82,38 +85,123 @@ function tagstringlink(seq, link, tag)
 end  	
 
 local db = {
-
 	["sports"] = { 
 	}
 }
-
+-- Returns the Levenshtein distance between the two given strings
+function string.levenshtein(str1, str2)
+	local len1 = string.len(str1)
+	local len2 = string.len(str2)
+	local matrix = {}
+	local cost = 0
+	
+        -- quick cut-offs to save time
+	if (len1 == 0) then
+		return len2
+	elseif (len2 == 0) then
+		return len1
+	elseif (str1 == str2) then
+		return 0
+	end
+	
+        -- initialise the base matrix values
+	for i = 0, len1, 1 do
+		matrix[i] = {}
+		matrix[i][0] = i
+	end
+	for j = 0, len2, 1 do
+		matrix[0][j] = j
+	end
+	
+        -- actual Levenshtein algorithm
+	for i = 1, len1, 1 do
+		for j = 1, len2, 1 do
+			if (str1:byte(i) == str2:byte(j)) then
+				cost = 0
+			else
+				cost = 1
+			end
+			
+			matrix[i][j] = math.min(matrix[i-1][j] + 1, matrix[i][j-1] + 1, matrix[i-1][j-1] + cost)
+		end
+	end
+	
+        -- return the last value - this is the Levenshtein distance
+	return matrix[len1][len2]
+end
 ---------------------Dialogue
-io.write('S: Bonjour, Je peux aussi vous aider à trouver le sport qui vous correspond le mieux. Quel est votre objectif? \n')
+--io.write(string.levenshtein("bonjour", "bonj"))
+
+io.write('U: ')
+	--print(type(io.lines('natation.txt')))
+ 
+io.write('\nS: Bonjour, je peux aussi vous aider à trouver le sport qui vous correspond le mieux. Quel est votre objectif? \n \n')
+local motProcheObjectif=false
+local motProcheGeneration=false
+local motProcheMaladie=false
+
 
 while true do
   io.write('U: ')
-	--print(type(io.lines('natation.txt')))
-  local word = io.read()
-  word = word:gsub("’", "'")
+  local word = io.read() 
+
+-- Distance de Levenshtein avec les objectifs 
+ for j=1, #listObjectifs do
+ 	for mot in word:gmatch("%w+") do
+  				if (string.levenshtein(mot, listObjectifs[j])==1) then
+  -- 			-- On conserve la valeur reelle du mot pour l'utiliser dans la base de donnees
+  -- 	 		table.insert(t, listObjectifs[j]) 
+  	 				motProcheObjectif = true
+  		end
+  end
+end
+
+-- Distance de Levenshtein avec l'age 
+
+ for j=1, #listGenerations do
+ 	for mot in word:gmatch("%w+") do
+  				if (string.levenshtein(mot, listGenerations[j])==1) then
+  -- 			-- On conserve la valeur reelle du mot pour l'utiliser dans la base de donnees
+  -- 	 		table.insert(t, listObjectifs[j]) 
+  	 				motProcheGeneration = true
+  		
+  end
+end
+end
+
+-- Distance de Levenshtein avec les contre-indications 
+
+ for j=1, #listMaladies do
+ 	for mot in word:gmatch("%w+") do
+  				if (string.levenshtein(mot, listMaladies[j])==1) then
+  -- 			-- On conserve la valeur reelle du mot pour l'utiliser dans la base de donnees
+  -- 	 		table.insert(t, listObjectifs[j]) 
+  	 				motProcheMaladie = true
+  		
+  end
+end
+
+
+    word = word:gsub("’", "'")
 	word = word:gsub("%p", " %0 ")
 	local seq = dark.sequence(word)
 	P(seq)
-	--print(seq:tostring(tags))
+	tagLeven = tagstringlink(seq, "#listeObjectifs", "#listeObjectifs" )
+  	if (havetag(seq, "#listeObjectifs") or (motProcheObjectif==true)) then
+    	io.write("\nS: Très bien, je peux vous proposer plusieurs sports! Etes vous plutot jeune ou âgé ? \n \n")
+    	motProcheObjectif=false
+   	elseif havetag(seq, "#listeGenerations") or (motProcheGeneration==true) then
+     	io.write("\nS: OK! Super, Vous etes jeune donc! Avez-vous des problèmes de santé ou des contre-indications ? \n \n")
+     	motProcheGeneration=false
+   	elseif havetag(seq, "#listesMaladies")  or (motProcheMaladie==true) then
+     	io.write("\nS: J’ai trouvé quelques sports qui peuvent vous correspondre.\n Je vous propose la natation.\n Voulez vous plus d'informations sur ce sport ?  \n \n")
+	 	motProcheMaladie=false
+	  -- db[sports] = db[sports] or {}
+	  -- print(serialize(db))
+  
+	end
 
-  	if havetag(seq, "#listeObjectifs") then
-    	io.write("S: Très bien, je peux vous proposer plusieurs sports! Etes vous plutot jeune ou âgé ? \n")
 
-    else if havetag(seq, "#listeGenerations") then
-    	io.write("S: OK! Super ! Avez-vous des problèmes de santé ou des contre-indications ? \n")
-
-   	else if havetag(seq, "#listesMaladies") then
-    	io.write("S: J’ai trouvé quelques sports qui peuvent vous correspondre. Je vous propose SPORTA, SPORTB ou encore SPORTC. Vous plus de détails sur ces sports ?  \n")
-
-
-	  db[sports] = db[sports] or {}
-	  print(serialize(db))
- 	  --db[monument].hauteur = mesure
-  end
 
   if line == nil then break end
   if line == "bye" then break end
@@ -127,6 +215,7 @@ while true do
   io.write('S: ', reponse, '!\n')
   io.write("S: Avez-vous une autre question? \n")
 
-end 
+
+
 end
 end
